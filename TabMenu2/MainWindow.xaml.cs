bbf736716247f633762af2636cb775111ec65861
Michael Smith <geometryomega@gmail.com>
2020-05-23 22:41:50 +1000

    improved drag and drop for image and tiles. also added gui formatting

--- TabMenu2/MainWindow.xaml.cs
+++ TabMenu2/MainWindow.xaml.cs
@@ -12,16 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Windows.Media.Animation;
 
 namespace TabMenu2
 {
     //to do
     //margin constants in xaml
+    //make tile panel grid width better
     public partial class MainWindow : Window
     {
         //declaring global variables
 
-        BitmapSource bmMain;
+        BitmapSource bmMain;        
 
         double xRatio = 1;
         double yRatio = 1;
@@ -39,10 +43,10 @@ namespace TabMenu2
         }
 
         //opens folder to select image upon mouse click
-        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        private void rectMain_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.Filter = "Images (.png)|*.png";
+            dlg.Filter = "Images (.png)|*.png";            
             Nullable<bool> result = dlg.ShowDialog();
             if (result == true)
             {
@@ -50,6 +54,26 @@ namespace TabMenu2
             }
         }
 
+        //loads image upon dragging and dropping it in
+        private void rectMain_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    // Note that you can have more than one file.
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                    // Assuming you have one file that you care about, pass it off to whatever
+                    // handling code you have defined.
+                    loadImage(files[0]);
+                }
+            }
+            catch (Exception e1) { }
+
+        }
+
+        //loads a provided image displays it on the main page
         void loadImage(string sFileName)
         {
             bmMain = new BitmapImage(new Uri(sFileName));
@@ -58,26 +82,157 @@ namespace TabMenu2
             xRatio = Convert.ToDouble(bmMain.PixelHeight) / Convert.ToDouble(bmMain.PixelWidth);
             yRatio = 1 / xRatio;
 
-            //xScaling.Text = Convert.ToString(bmMain.PixelWidth);
-            //yScaling.Text = Convert.ToString(bmMain.PixelHeight);
+            xScaling.Text = Convert.ToString(bmMain.PixelWidth);
+            yScaling.Text = Convert.ToString(bmMain.PixelHeight);
 
             imageLoaded = true;
             //tryEnablingBoxes();
 
             rectMain.Visibility = Visibility.Hidden;
+
         }
 
-        private void Rectangle_Drop(object sender, DragEventArgs e)
+        private void rectTilesPreview_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Images (.png)|*.png";
+            dlg.Multiselect = true;            
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
             {
-                // Note that you can have more than one file.
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
 
-                // Assuming you have one file that you care about, pass it off to whatever
-                // handling code you have defined.
-                loadImage(files[0]);
+                rectTilesPreview.Visibility = Visibility.Hidden;
+                textTilesPreview.Visibility = Visibility.Hidden;
+
+                int row = 0;
+                int col = 0;
+
+                foreach (string fileName in dlg.FileNames)
+                {
+                    ProcessTile(fileName, row, col);
+                    col++;
+                    if (col == 3)
+                    {
+                        col = 0;
+                        row++;
+                        var rowDefinition = new RowDefinition();
+                        rowDefinition.Height = GridLength.Auto;
+                        tilesPreviewGrid.RowDefinitions.Add(rowDefinition);
+                    }
+                }
+
+                tilesLoaded = true;
+                //tryEnablingBoxes();
             }
         }
+
+        void ProcessTile(string fileName, int row, int col)
+        {
+            Console.WriteLine(fileName);
+            // create a new image for each tile and add it to the stackPanel
+
+            Image newTileImage = new Image();
+
+            BitmapSource bmTile = new BitmapImage(new Uri(fileName));
+            newTileImage.Source = bmTile;
+            newTileImage.Height = tilesImageSize - tilesMargin;
+            newTileImage.Width = tilesImageSize;
+            newTileImage.Stretch = Stretch.Fill;
+            newTileImage.Margin = new Thickness(2,2,2,2);
+
+            
+
+
+            tilesPreviewGrid.Children.Add(newTileImage);
+            Grid.SetColumn(newTileImage, col);
+            Grid.SetRow(newTileImage, row);
+
+
+
+
+
+        }
+
+        private void rectTilesPreview_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    // Note that you can have more than one file.
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                    
+                }
+            }
+            catch (Exception e1) { }
+
+        }
+
+        private void rectMain_DragEnter(object sender, DragEventArgs e)
+        {
+            ColorAnimation ca = new ColorAnimation((Color)ColorConverter.ConvertFromString("#f0f0f0"), new Duration(TimeSpan.FromSeconds(0.2)));
+            rectMain.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#dddddd"));
+            rectMain.Fill.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+        }
+
+        private void rectMain_DragLeave(object sender, DragEventArgs e)
+        {
+            ColorAnimation ca = new ColorAnimation((Color)ColorConverter.ConvertFromString("#dddddd"), new Duration(TimeSpan.FromSeconds(0.2)));
+            rectMain.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f0f0f0"));
+            rectMain.Fill.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+        }
+
+        private void rectTilesPreview_DragEnter(object sender, DragEventArgs e)
+        {
+            ColorAnimation ca = new ColorAnimation((Color)ColorConverter.ConvertFromString("#f0f0f0"), new Duration(TimeSpan.FromSeconds(0.2)));
+            rectTilesPreview.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#dddddd"));
+            rectTilesPreview.Fill.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+        }
+
+        private void rectTilesPreview_DragLeave(object sender, DragEventArgs e)
+        {
+            ColorAnimation ca = new ColorAnimation((Color)ColorConverter.ConvertFromString("#dddddd"), new Duration(TimeSpan.FromSeconds(0.2)));
+            rectTilesPreview.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f0f0f0"));
+            rectTilesPreview.Fill.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+        }
+
+        private void xScaling_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void yScaling_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void xScaling_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (xScaling.IsFocused)
+                {
+                    yScaling.Text = Convert.ToString(Math.Floor(Convert.ToInt32(xScaling.Text) * xRatio));
+                }
+            }
+            catch (System.FormatException e1) { }
+        }
+
+        private void yScaling_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (yScaling.IsFocused)
+                {
+                    xScaling.Text = Convert.ToString(Math.Floor(Convert.ToInt32(yScaling.Text) * yRatio));
+                }
+            }
+            catch (System.FormatException e1) { }
+        }
+
+      
     }
 }
